e72e61b22b07882e9a31ad835f9f606c45007427
StarsAll_InFrost <dlycn749007@outlook.com>
2026-09-25 18:38:16 +0800

    pass

--- src/main.py
+++ src/main.py
@@ -45,9 +45,7 @@ if __name__ == "__main__":
         "这是刻意的设计：你需要知道每一步在做什么。"
     )
     from aichat_histdl import Downloader
+    import config
 
-    TOKEN = (
-        "vZo78lz4Ztz8Aa0vnfBFCRqM4YHETX+xQ6sbyD2q4mx2RUn5ufHTJF6yXGEcDb2T"  # 替换为最新的
-    )
-    COOKIE_STRING = "smidV2=20251118024359c361ae887274b6c4698b8d53ecb2a03b007d1c54904c8b4a0; .thumbcache_6b2e5483f9d858d7c661c5e276b6a6ae=C8Vgz2OpkSxr+HCKKbP9jD/Ci6FqC/XliaubAP4HdHyfnMgdP7iaoabG4k368rE4LrwBvm/HP1oMYYktX3CQ/A%3D%3D; intercom-device-id-guh50jw4=28285566-7731-4758-b4ba-489b53eb8db7; HWWAFSESID=e49cc2626acbdf84144; HWWAFSESTIME=1773638457742; ds_session_id=c786dd2be64e456484d64e62162c8651"
-    Downloader(token=TOKEN, cookie=COOKIE_STRING).run()
+
+    Downloader(token=config.TOKEN, cookie=config.COOKIE_STRING).run()
