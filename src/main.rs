eba77a411d8f6e9f6a71b80571e711280c1a6b6e
StarsAll_InFrost <dlycn749007@outlook.com>
2026-09-26 02:14:47 +0800

    pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod zhihu;
+mod config;
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    let urls = zhihu::init_urls();
+    let body = zhihu::get_from_id(urls.get("question").unwrap()).await;
+    let questions = zhihu::get_question_list(body);
+    for [url, title] in questions {
+        println!("{}: {}", url, title);
+    }
 }
---
+++ src/zhihu/mod.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+use reqwest;
+use serde_json::Value;
+use crate::config;
+
+
+pub fn init_urls() -> HashMap<&'static str, &'static str> {
+    let mut urls = HashMap::new();
+    urls.insert("home", "https://www.zhihu.com/");
+    urls.insert(
+        "voter",
+        "https://www.zhihu.com/api/v4/articles/2085374500649096302/voters",
+    );
+    urls.insert(
+        "question",
+        "https://www.zhihu.com/api/v4/creators/question_route/author_related/recommend?limit=200&offset=0&page_source=web_author_recommend&recom_domain_score_ab=1",
+    );
+    urls.insert(
+        "answer",
+        "https://www.zhihu.com/creator/featured-question/invited",
+    );
+    urls.insert("write", "https://zhuanlan.zhihu.com/write");
+    urls
+}
+
+pub async fn get_from_id(url: &str) -> String {
+    let client = reqwest::Client::new();
+    let resp = client
+        .get(url)
+        .headers(config::build_headers())
+        .send()
+        .await
+        .unwrap();
+    let body = resp.text().await.unwrap();
+    body
+}
+
+pub fn get_question_list(base: String) -> Vec<[String;2]> {
+    let mut article_list = Vec::new();
+    let json: Value = serde_json::from_str(&base).unwrap();
+    if let Some(data) = json["data"].as_array() {
+        for item in data {
+            if let (Some(url), Some(title)) =
+                (item["question"]["id"].as_str(), item["question"]["title"].as_str())
+            {
+                article_list.push([
+                    url.to_string(),
+                    title.to_string(),
+                ]);
+            }
+        }
+    }
+    article_list
+}
